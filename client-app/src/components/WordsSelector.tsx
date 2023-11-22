6b1c8fec9da2827c0aa080ab96226d9797bde535
Andrii Tagaiev <atagaiev@taskmaverick.com>
2023-11-22 14:30:26 +0100

    Added logic to show words   in order to let user to select them for analysis

--- client-app/src/components/WordsSelector.tsx
+++ client-app/src/components/WordsSelector.tsx
@@ -1,28 +1,49 @@
-export default function WordsSelector() {
+import { Word } from "../interfaces/Word";
+
+interface WordsSelectorProps {
+    initialWordsToSelect: Word[];
+}
+
+const WordsSelector: React.FC<WordsSelectorProps> = ({ initialWordsToSelect }) => {
+    const groupWordsByLine = (words: Word[]) => {
+        return words.reduce((groupedWords, word) => {
+            const lineNumber = word.lineNumber;
+            if (!groupedWords[lineNumber]) {
+                groupedWords[lineNumber] = [];
+            }
+            groupedWords[lineNumber].push(word);
+            return groupedWords;
+        }, {} as Record<number, Word[]>);
+    };
+
+    const wordsGrouped = groupWordsByLine(initialWordsToSelect);
     return (
-      <div className="container mt-4">
-        <div className="row">
-          <section>
-            <h2>Select words to analyze</h2>
-  
-  
-            <div className="col-md-6 text-left">
-              <p>
-                <button type="button" className="btn btn-primary">Храня</button>
-                <button type="button" className="btn btn-primary">убогое</button>
-                <button type="button" className="btn btn-success">молчанье</button>
-              </p>
-              <p>
-                <button type="button" className="btn btn-primary">Над</button>
-                <button type="button" className="btn btn-success">сумраком</button>
-                <button type="button" className="btn btn-primary">гниющих</button>
-                <button type="button" className="btn btn-success">вод</button>
-              </p>
-              <p>
-              </p>
+        <div className="container mt-4">
+            <div className="row">
+                <section>
+                    <h2>Select words to analyze</h2>
+                    <div className="col-md-6 text-left">
+                        {Object.keys(wordsGrouped).map((lineNumberString) => {
+                            const lineNumber = parseInt(lineNumberString, 10);
+                            return (
+                                <p key={lineNumber}>
+                                    {wordsGrouped[lineNumber].map((word) => (
+                                        <button
+                                            key={word.value}
+                                            type="button"
+                                            className="btn btn-light"
+                                        >
+                                            {word.value}
+                                        </button>
+                                    ))}
+                                </p>
+                            );
+                        })}
+                    </div>
+                </section>
             </div>
-          </section>
         </div>
-      </div>
     )
-  }
+}
+
+export default WordsSelector;
--- client-app/src/pages/Home.tsx
+++ client-app/src/pages/Home.tsx
@@ -5,22 +5,24 @@ import WordsInitialAssociation from "../components/WordsInitialAssociation";
 import WordsSelector from "../components/WordsSelector";
 import FinalCrossWordsAssociation from "../components/FinalCrossWordsAssociation";
 import { WordAnalysis } from "../interfaces/WordAnalysis";
+import { useState } from 'react';
 
 export default function Home() {
-    const onStartAnalysis = (data: WordAnalysis) => {
-        // Handle the analysis logic here
-        console.log('Analysis started with data:', data);
-      };
-      
-      const initialValues: WordAnalysis = {
+    const [analysis, setAnalysis] = useState<WordAnalysis>({
         title: '',
         text: '',
         words: []
-    };
+    })
+    const onStartAnalysis = (analysis: WordAnalysis) => {
+        // Handle the analysis logic here
+        console.log('Analysis started with data:', analysis);
+        setAnalysis(analysis);
+      };
+      
     return (
         <>
-          <TextInput  onStartAnalysis={onStartAnalysis} initialAnalysisData={initialValues}/>
-          <WordsSelector />
+          <TextInput  onStartAnalysis={onStartAnalysis} initialAnalysisData={analysis}/>
+          <WordsSelector initialWordsToSelect={analysis.words}/>
           <WordsInitialAssociation />
           <WordsAssociation />
           <CrossWordsAssociation />
